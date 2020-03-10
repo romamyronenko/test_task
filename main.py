6a9339901d0810aa3d5c1a4a09fdac46bb3fe7b3
romamyronenko <eoma575@gmail.com>
2020-03-10 02:17:02 +0200

    Added comments

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import random
 
 
 def game(n):
+    """Simulate a game in "The Chase" and return amount that the winner received."""
     bank = 0
 
     def step(player, n):
+        """Return next player with dice."""
         r = random.randint(1, 6)
         if r < 3:
             return (player + 1) % n
@@ -12,8 +14,10 @@ def game(n):
             return (player - 1) % n
         return player
     for i in range(n-1, -1, -1):
-        s = 0
+        s = 0 # turns
+        # beginning of round, 2 random players with dice
         players_with_dices = [random.randint(0, i), random.randint(0, i)]
+        # break when one player has two dieces
         while players_with_dices[0] != players_with_dices[1]:
             players_with_dices[0] = step(players_with_dices[0], i+1)
             players_with_dices[1] = step(players_with_dices[1], i+1)
